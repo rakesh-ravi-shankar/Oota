17a8ebdb3f6087730f75233f0565f3396ac831e1
monicamalkani <malkanimonica@gmail.com>
2017-04-04 23:20:03 -0400

    config

--- public/config.js
+++ public/config.js
@@ -26,16 +26,6 @@
                 controller: 'ReviewOrderController',
                 controllerAs: 'model'
             })
-            // .when("/login", {
-            //     templateUrl: 'views/login.view.client.html',
-            //     controller: 'LoginController',
-            //     controllerAs: 'model'
-            // })
-            // .when("/registerUser", {
-            //     templateUrl: 'views/registerUser.view.client.html',
-            //     controller: 'RegisterUserController',
-            //     controllerAs: 'model'
-            // })
             .when("/registerRestaurant", {
                 templateUrl: 'views/registerRestaurant.view.client.html',
                 controller: 'RegisterRestaurantController',
@@ -45,6 +35,10 @@
                 templateUrl: 'views/deliver.view.client.html',
                 controller: 'DeliverController',
                 controllerAs: 'model'
+            })
+            .when("/userProfile", {
+                templateUrl: 'views/userProfile.view.client.html',
+
             })
             .otherwise({
                 templateUrl: 'views/login.view.client.html',
