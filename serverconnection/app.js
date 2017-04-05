d33a526a1d3299bc973d4f4b3efa2337d1786bb6
vinaynadagoud <nadagoud.v@husky.neu.edu>
2017-04-05 17:51:17 -0400

    creating order

--- serverconnection/app.js
+++ serverconnection/app.js
@@ -6,6 +6,7 @@ module.exports=function(app){
 
     require("./services/user/user.service.server")(app);
     require("./services/restaurant/restaurant.service.server")(app);
+    require("./services/order/order.service.server")(app);
     //require("./services/restaurant/restaurant.service.server")(app);
 
 
