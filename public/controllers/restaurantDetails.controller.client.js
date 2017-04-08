a75005897346897c61a66e8e04723f24cd62d2ca
monicamalkani <malkanimonica@gmail.com>
2017-04-07 23:04:41 -0400

    ui changes&restauant details

---
+++ public/controllers/restaurantDetails.controller.client.js
@@ -0,0 +1,19 @@
+/**
+ * Created by monica on 3/19/17.
+ */
+(function () {
+    angular
+        .module("Oota")
+        .controller("RestaurantDetailController", RestaurantDetailController);
+
+
+    function RestaurantDetailController($routeParams, $window, $location, RestaurantListService) {
+        var vm = this;
+        function init() {
+
+
+        }
+        init();
+        
+    }
+})();
