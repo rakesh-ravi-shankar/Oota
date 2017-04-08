b8585c0a90fd2690e31000a865d7d25a513d496b
vinaynadagoud <nadagoud.v@husky.neu.edu>
2017-04-08 01:31:23 -0400

    session started

--- serverconnection/services/user/user.service.server.js
+++ serverconnection/services/user/user.service.server.js
@@ -1,20 +1,98 @@
 module.exports=function(app){
 
     var userModel =require("../../model/user/user.model.server");
+    var passport      = require('passport');
+    var LocalStrategy = require('passport-local').Strategy;
+    passport.use(new LocalStrategy(localStrategy));
+    passport.serializeUser(serializeUser);
+    passport.deserializeUser(deserializeUser);
 
-    app.get("/api/user",findUser);
-    app.post("/api/user",createUser);
-    app.get("/api/user/:uid",findUserById);
+    var auth = authorized;
+    app.post  ('/api/login', passport.authenticate('local'), login);
+    app.post  ('/api/logout',         logout);
+    app.post  ('/api/register',       register);
+    //app.post  ('/api/user',     auth, createUser);
+    //app.get   ('/api/loggedin',       loggedin);
+    //app.get   ('/api/user',     auth, findAllUsers);
+    //app.put   ('/api/user/:id', auth, updateUser);
+    //app.delete('/api/user/:id', auth, deleteUser);
+
+    //app.get("/api/user",findUser);
+    //app.post("/api/user",createUser);
+    //app.get("/api/user/:uid",findUserById);
     // app.put("/api/user/:uid",updateUser);
     // app.delete("/api/user/:uid",deleteUser);
 
+    function authorized (req, res, next) {
+        if (!req.isAuthenticated()) {
+            res.send(401);
+        } else {
+            next();
+        }
+    }
 
-    function createUser(req,res) {
+    function serializeUser(user, done) {
+        done(null, user);
+    }
+
+    function deserializeUser(user, done) {
+        userModel
+            .findUserById(user._id)
+            .then(
+                function(user){
+                    done(null, user);
+                },
+                function(err){
+                    done(err, null);
+                }
+            );
+    }
+
+    function localStrategy(username, password, done) {
+        userModel
+            .findUserByCredentials(username, password)
+            .then(
+                function(user) {
+                    if (!user) { return done(null, false); }
+                    return done(null, user);
+                },
+                function(err) {
+                    if (err) { return done(err); }
+                }
+            );
+    }
+
+
+
+    function login(req, res) {
+        var user = req.user;
+        res.json(user);
+    }
+
+    function logout(req, res) {
+        req.logOut();
+        res.send(200);
+    }
+
+    function loggedin(req, res) {
+        res.send(req.isAuthenticated() ? req.user : '0');
+    }
+
+
+    function register(req,res) {
         var newUser = req.body;
         userModel
             .createUser(newUser)
             .then(function(user) {
-                res.json(user);
+                if(user){
+                    req.login(user, function(err) {
+                        if(err) {
+                            res.status(400).send(err);
+                        } else {
+                            res.json(user);
+                        }
+                    });
+                }
             }, function (error) {
                 res.sendStatus(500).send(error);
             });
