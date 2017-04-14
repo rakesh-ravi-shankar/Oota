7d70cd33a0074e1def9efe6eb116fe408e504ea2
Rakesh Ravi Shankar <Rakesh@Rakeshs-MacBook-Pro.local>
2017-04-14 04:50:48 -0400

    Authentication to delivery signin

--- public/controllers/login.controller.client.js
+++ public/controllers/login.controller.client.js
@@ -27,35 +27,87 @@
         // }
         function loginUser(user)
         {
-            // console.log("bhla");
-            UserService
-                .findUserByCredentials(user)
-                .success(function (loggedUser) {
-                    console.log(loggedUser);
-                    if(loggedUser)
-                    {
+            vm.error="";
+            if (typeof user === "undefined") {
+                vm.error = "Undefined Entry!";
+            }
+            else if ((typeof user.username == "undefined") || (user.username == ""))
+            {
+                vm.error = "Please enter username!";
+            }
+            else if ((typeof user.password == "undefined") || (user.password == ""))
+            {
+                vm.error = "Please enter password!";
+            }
+            else {
+                UserService
+                    .findUserByCredentials(user)
+                    .success(function (loggedUser) {
                         console.log(loggedUser);
+                        if (loggedUser) {
+                            console.log(loggedUser);
 
-                        $rootScope.user = loggedUser;
-                        vm.user= $rootScope.user;
-                        console.log("logged in user " + $rootScope.user);
-                        $location.url("/deliver" );
-                    }
-                    else
-                    {
-                        vm.message="user not found";
-                    }
-                }) .error(function (err) {
-                vm.message="user not found";
-            });
+                            $rootScope.user = loggedUser;
+                            vm.user = $rootScope.user;
+                            console.log("logged in user " + $rootScope.user);
+                            $location.url("/deliver");
+                        }
+                        else {
+                            vm.error = "User not found!";
+                        }
+                    }).error(function (err) {
+                    vm.error = "User not found!";
+                });
+            }
         }
 
         function registerUser(user) {
 
-            UserService.findUserByUsername(user.username)
-                .success(function (u) {
-                    if(u.length == 0)
-                    {
+            vm.register_error = "";
+            vm.register_message = "";
+
+            if (typeof user === "undefined") {
+                vm.register_error = "Undefined Entry!";
+            }
+            else if ((typeof user.username == "undefined") || (user.username == ""))
+            {
+                vm.register_error = "Please enter username!";
+            }
+            else if ((typeof user.password == "undefined") || (user.password == ""))
+            {
+                vm.register_error = "Please enter password!";
+            }
+            else if (user.password !== user.verify_password) {
+                vm.register_error = "Password mismatch!";
+            }
+            else {
+                UserService.findUserByUsername(user.username)
+                    .success(function (u) {
+                        if(u.length == 0)
+                        {
+                            UserService.createUser(user)
+                                .success(function (user) {
+                                    if (user) {
+                                        $("#validateUserModal").modal("hide");
+                                        $("body").removeClass("modal-open");
+                                        $(".modal-backdrop").remove();
+                                        $rootScope.user = user;
+                                        vm.user= $rootScope.user;
+                                        //$location.url("/restaurantList/" + vm.restaurantId + "/restaurantMenu/");
+
+                                        vm.register_message = "User created successfully!";
+                                    }
+                                    else
+                                        vm.register_error = "User creation failed!"
+                                });
+                        }
+                        else {
+                            vm.register_error="User name already taken!";
+                        }
+
+
+                    })
+                    .error(function (err) {
                         // vm.message="User name NOT taken!";
                         UserService.createUser(user)
                             .success(function (user) {
@@ -65,42 +117,16 @@
                                     $(".modal-backdrop").remove();
                                     $rootScope.user = user;
                                     vm.user= $rootScope.user;
-                                    //$location.url("/restaurantList/" + vm.restaurantId + "/restaurantMenu/");
+                                    //$location.url("/restaurantList/" + vm.restaurantId + "/restaurantMenu/" + $rootScope.currentUser.username + "/order");
 
-                                    vm.message = "User created successfully";
+                                    vm.register_message = "User created successfully";
                                 }
                                 else
-                                    vm.error = "User not created successfully!"
+                                    vm.register_error = "User creation failed!"
                             });
-                    }
-                    else {
-                        vm.error="User name already taken!";
-                        // console.log("EXISTS")
-                    }
 
-
-                })
-                .error(function (err) {
-                    // vm.message="User name NOT taken!";
-                    UserService.createUser(user)
-                        .success(function (user) {
-                            if (user) {
-                                $("#validateUserModal").modal("hide");
-                                $("body").removeClass("modal-open");
-                                $(".modal-backdrop").remove();
-                                $rootScope.user = user;
-                                vm.user= $rootScope.user;
-                                //$location.url("/restaurantList/" + vm.restaurantId + "/restaurantMenu/" + $rootScope.currentUser.username + "/order");
-
-                                vm.message = "User created successfully";
-                            }
-                            else
-                                vm.error = "User not created successfully!"
-                        });
-
-                });
-
-
+                    });
+            }
 
         }
 
