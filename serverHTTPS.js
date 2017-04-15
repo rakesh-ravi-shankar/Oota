b6e85db8ec06cfd9f67a0b850da6b7dc280f5529
Rakesh Ravi Shankar <Rakesh@Rakeshs-MacBook-Pro.local>
2017-04-15 02:21:11 -0400

    added https server implementation

---
+++ serverHTTPS.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Rakesh on 4/15/17.
+ */
+var express = require('express');
+var https = require('https');
+var http  = require('http');
+var fs = require('fs');
+
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var passport      = require('passport');
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+require ("./setup/app.js")(app);
+
+var options = {
+    key: fs.readFileSync('setup/keys/agent2-key.pem'),
+    cert: fs.readFileSync('setup/keys/agent2-cert.pem')
+};
+
+var server = require('./server/app.js');
+server(app);
+
+https.createServer(options, app).listen(8443);
+http.createServer(function (req, res) {
+    site= req.headers['host'];
+    site = site.split(':')[0];
+    site=site+':8443';
+    res.writeHead(301, { "Location": "https://" + site + req.url });
+    res.end();
+}).listen(9000);
